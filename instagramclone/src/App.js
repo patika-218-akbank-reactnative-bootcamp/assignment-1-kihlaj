554472577775beba1f872f3d559affdefd1a05e6
behzat bilgin erdem <bbilgin.erdem@gmail.com>
2022-08-16 19:00:15 +0300

    instagram logo in png and svg added & App.js moved inside src
    Components folder added and Banner and UserCard added

---
+++ instagramclone/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {SafeAreaView, ScrollView, StatusBar, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <View>
+          <Text>xD</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ instagramclone/src/components/Banner.js
@@ -0,0 +1,11 @@
+import {View, Text, Image, StyleSheet} from 'react-native';
+
+const Banner = () => {
+  return <View>
+    {/* <Image source={require('../../assets/banner.jpg')} style={styles.image} /> */}
+  </View>;
+};
+
+const styles = StyleSheet.create({
+  container:
+})
