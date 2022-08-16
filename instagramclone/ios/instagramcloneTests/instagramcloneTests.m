6241952b13e6978b0b4cd1841369f94557daaee3
behzat bilgin erdem <bbilgin.erdem@gmail.com>
2022-08-16 14:39:10 +0300

    name changed

--- instagramclone/ios/instagramcloneTests/instagramcloneTests.m
+++ instagramclone/ios/instagramcloneTests/instagramcloneTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface instagramcloneTests : XCTestCase
+@interface InstagramCloneTests : XCTestCase
 
 @end
 
-@implementation instagramcloneTests
+@implementation InstagramCloneTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
