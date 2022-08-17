da76098979ec2823d4da9960204d9cca4211cac0
behzat bilgin erdem <bbilgin.erdem@gmail.com>
2022-08-17 17:38:13 +0300

    banner styles reorganized

--- instagramclone/src/components/home/Banner/Banner.js
+++ instagramclone/src/components/home/Banner/Banner.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  Image,
-  StyleSheet,
-  SafeAreaView,
-  Pressable,
-} from 'react-native';
+import {View, Text, Image, StyleSheet, Pressable} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import styles from './Banner.style';
 
 const Banner = ({children}) => {
   return (
@@ -50,40 +45,5 @@ const Banner = ({children}) => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    flexDirection: 'row',
-    marginHorizontal: 20,
-  },
-  logo: {
-    width: 100,
-    height: 50,
-    resizeMode: 'contain',
-  },
-  icons: {
-    flexDirection: 'row',
-  },
-  icon: {
-    width: 25,
-    height: 25,
-    marginLeft: 8,
-    resizeMode: 'contain',
-  },
-  unreadBadge: {
-    backgroundColor: '#ff5456',
-    position: 'absolute',
-    left: 20,
-    bottom: 15,
-    width: 20,
-    height: 20,
-    borderRadius: 25,
-    alignItems: 'center',
-    justifyContent: 'center',
-    zIndex: 100,
-  },
-});
 
 export default Banner;
--- instagramclone/src/components/home/Banner/Banner.style.js
+++ instagramclone/src/components/home/Banner/Banner.style.js
@@ -1,3 +1,36 @@
 import {StyleSheet} from 'react-native';
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  container: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginHorizontal: 20,
+  },
+  logo: {
+    width: 100,
+    height: 50,
+    resizeMode: 'contain',
+  },
+  icons: {
+    flexDirection: 'row',
+  },
+  icon: {
+    width: 25,
+    height: 25,
+    marginLeft: 8,
+    resizeMode: 'contain',
+  },
+  unreadBadge: {
+    backgroundColor: '#ff5456',
+    position: 'absolute',
+    left: 20,
+    bottom: 15,
+    width: 20,
+    height: 20,
+    borderRadius: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+    zIndex: 100,
+  },
+});
